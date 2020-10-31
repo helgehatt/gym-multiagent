36175cce7152347b647c4bbe2bd053eebbe91a4d
Helge Hatteland <helge.hatteland@sonat.no>
2020-10-31 17:49:07 +0100

    Added Point class

---
+++ gym_multiagent/point.py
@@ -0,0 +1,22 @@
+
+class Point(tuple):
+
+    def __new__(self, x, y):
+        return tuple.__new__(Point, (x, y))
+
+    @property
+    def x(self):
+        return self.__getitem__(0)
+
+    @property
+    def y(self):
+        return self.__getitem__(1)
+
+    def __add__(self, other):
+        return Point(self.x + other.x, self.y + other.y)
+
+    def __sub__(self, other):
+        return Point(self.x - other.x, self.y - other.y)
+
+    def __repr__(self):
+        return f"Point{tuple.__repr__(self)}"
