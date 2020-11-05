a436f74fdc5574cf89c5d30b292a2cbff84cc4bf
Helge Hatteland <helge.hatteland@sonat.no>
2020-11-05 11:03:29 +0100

    Updated setup.py

--- setup.py
+++ setup.py
@@ -1,7 +1,20 @@
-from setuptools import setup
+from setuptools import setup, find_packages
 
 setup(
-      name='gym_multiagent',
-      version='1.0.0',
-      install_requires=['gym']  # And any other dependencies foo needs
+    name="gym_multiagent",
+    version="1.0.0",
+    license="MIT",
+    description="Sokoban-inspired multi-agent environment for OpenAI Gym",
+    author="helgehatt",
+    url="https://github.com/helgehatt/gym-multiagent",
+    keywords=["multiagent", "openai", "gym", "environment"],
+    install_requires=["gym", "numpy"],
+    packages=find_packages(),
+    classifiers=[
+        "Development Status :: 3 - Alpha",
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: MIT License",
+        "Programming Language :: Python :: 3",
+        "Operating System :: OS Independent",
+    ],
 )
